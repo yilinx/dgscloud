4ac4fce0d2b88fca09f24c8d4a85f86df65bfad0
yilinx <yilinx@users.noreply.github.com>
2018-01-22 18:04:39 +0800

    Add files via upload

---
+++ chronos.py
@@ -0,0 +1,26 @@
+
+# coding: utf-8
+
+# Weekly automator
+# 
+# For a weekly update, if the date difference between reference date and today is a multiple of 7 (% 7 == 0), it will run the script.
+
+# In[ ]:
+
+
+#!/usr/bin/python2.7
+
+import datetime
+
+# d1 is the reference date to run the extraction scripts. Ideally should be Saturday wee hours.
+d1 = datetime.date(2018,1,20)
+d2 = datetime.date.today()
+
+diff = d2-d1
+
+if diff.days % 2 == 0:
+    import pipeline
+    print "Start Data Extraction!!"
+else:
+    print "Not running script today."
+
